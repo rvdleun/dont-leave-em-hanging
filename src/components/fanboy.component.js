4ea59e049662af38b5faf934b140d794b532140d
Ruben van der Leun <ruben@leunix.nl>
2020-04-13 22:32:33 +0200

    feature(fanboy): Added feedback animations and text

--- src/components/fanboy.component.js
+++ src/components/fanboy.component.js
@@ -1,3 +1,5 @@
+const approvedText = ['YEAH!', 'WOO!', 'NICE!', 'COOL!', 'AWESOME!'];
+
 AFRAME.registerComponent('fanboy', {
     schema: {
         distance: { type: 'number', default: null },
@@ -24,7 +26,35 @@ AFRAME.registerComponent('fanboy', {
                 (this.data.type === 'fist' && velocity < .05);
             const approved = rightHand && rightType && rightVelocity;
 
-            this.faceEl.setAttribute('src', `#${approved ? '' : 'not-'}approved-1`);
+            if (!rightHand) {
+                this.textEl.setAttribute('value', 'Wrong hand');
+            } else if (!rightType) {
+                this.textEl.setAttribute('value', (this.data.type === 'hand' ? 'High five' : 'Fist bump') + '\nrequested');
+            } else if(!rightVelocity) {
+                this.textEl.setAttribute('value', (this.data.type === 'hand' ? 'Too slow' : 'Too fast'));
+            } else {
+                this.textEl.setAttribute('value', approvedText[Math.floor(Math.random() * approvedText.length)]);
+            }
+
+            const faceSrc = approved ?
+                `#approved-${Math.floor(Math.random() * 5) + 1}` :
+                `#not-approved-${Math.floor(Math.random() * 3) + 1}`;
+            this.handEl.setAttribute('animation__color', {
+                property: 'material.color',
+                to: approved ? '#0f0' : '#f00',
+                dur: 500,
+                easing: 'linear'
+            });
+            this.textEl.setAttribute('animation__color', {
+                property: 'color',
+                from: approved ? '#070' : '#700',
+                to: approved ? '#0f0' : '#f00',
+                dur: 1000,
+                easing: 'linear'
+            });
+            this.textEl.setAttribute('animation__opacity', 'property: opacity; from: 0; to: 1; dur: 250; easing: linear');
+
+            this.faceEl.setAttribute('src', faceSrc);
             this.el.emit('removeFanboy', { emitLaneAvailable: true });
         },
 
@@ -33,6 +63,7 @@ AFRAME.registerComponent('fanboy', {
 
             this.faceEl.setAttribute('animation', 'property: opacity; from: 1; to: 0; dur: 1000; delay: 1000');
             this.handEl.setAttribute('animation', 'property: opacity; from: 1; to: 0; dur: 300; easing: linear');
+            this.textEl.setAttribute('animation__opacity_gone', 'property: opacity; from: 1; to: 0; dur: 250; delay: 2000; easing: linear');
 
             setTimeout(() => {
                 this.el.parentNode.parentNode.removeChild(this.el.parentNode);
@@ -40,7 +71,7 @@ AFRAME.registerComponent('fanboy', {
                 if (e.detail.emitLaneAvailable) {
                     this.el.emit('laneAvailable', { lane });
                 }
-            }, 1250 + (Math.random() * 500));
+            }, 2250 + (Math.random() * 500));
         }
     },
 
@@ -48,6 +79,7 @@ AFRAME.registerComponent('fanboy', {
         const { hand, lane, type } = this.data;
         this.ready = false;
 
+        const faceSrc = Math.floor(Math.random()) * 100 === 1 ? 'vincent' : (Math.floor(Math.random() * 3) + 1).toString();
         const face = document.createElement('a-plane');
         face.setAttribute('animation', {
             property: 'opacity',
@@ -69,12 +101,17 @@ AFRAME.registerComponent('fanboy', {
         face.setAttribute('opacity', '0');
         face.setAttribute('position', '0 .2 0');
         face.setAttribute('scale', '.25 .25 .25');
-        face.setAttribute('src', '#waiting-1');
+        face.setAttribute('src', `#waiting-${faceSrc}`);
         face.setAttribute('transparent', 'true');
         this.el.appendChild(face);
 
         const handEl = document.createElement('a-plane');
         const xUpdate = hand === 'left' ? -1 : 1;
+        const handPosition = {
+            x: (.1 + (Math.random() / 10)) * -xUpdate,
+            y: .1 + (type === 'hand' ? .25 : 0),
+            z: .05
+        };
         const handRotation = Math.random() * 40 * xUpdate;
         handEl.setAttribute('animation', {
             property: 'opacity',
@@ -91,11 +128,7 @@ AFRAME.registerComponent('fanboy', {
             loop: true,
         });
         handEl.setAttribute('opacity', '0');
-        handEl.setAttribute('position', {
-            x: (.1 + (Math.random() / 10)) * -xUpdate,
-            y: .1 + (type === 'hand' ? .25 : 0),
-            z: .05
-        });
+        handEl.setAttribute('position', handPosition);
         handEl.setAttribute('rotation', {
             x: 0,
             y: 0,
@@ -114,9 +147,25 @@ AFRAME.registerComponent('fanboy', {
         handEl.setAttribute('transparent', 'false');
         this.el.appendChild(handEl);
 
-        setTimeout(() => {
-            this.ready = true;
-        }, 250);
+        const textEl = document.createElement('a-text');
+        textEl.setAttribute('align', 'center');
+        textEl.setAttribute('opacity', '0');
+        textEl.setAttribute('position', {
+            x: handPosition.x,
+            y: handPosition.y,
+            z: handPosition.z + .05,
+        });
+        textEl.setAttribute('animation__scale', {
+            property: 'scale',
+            from: '.275 .275 .1',
+            to: '.3 .3 .1',
+            dir: 'alternate',
+            dur: 500,
+            easing: 'linear',
+            loop: true,
+        });
+        textEl.setAttribute('value', 'TEST');
+        this.el.appendChild(textEl);
 
         const receiver = document.createElement('a-box');
         receiver.setAttribute('dynamic-body', 'mass: 0; linearDamping: .99995; angularDamping: .999995;');
@@ -129,6 +178,22 @@ AFRAME.registerComponent('fanboy', {
 
         this.faceEl = face;
         this.handEl = handEl;
+        this.textEl = textEl;
+
+        setTimeout(() => {
+            this.ready = true;
+        }, 250);
+        //
+        // setTimeout(() => {
+        //     const playerHand = document.createElement('a-entity');
+        //     playerHand.classList.add(this.data.hand === 'left' ? 'right' : 'left');
+        //     playerHand.classList.add(this.data.type);
+        //
+        //     this.el.emit('contact', {
+        //         playerHand,
+        //         velocity: 5,
+        //     });
+        // }, 3000);
     },
 
     update: function() {
